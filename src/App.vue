488f16df2635bf2355e9a71f4bdcf5365ce5086f
Shubidumdu <skaty4870@naver.com>
2021-10-08 00:13:35 +0900

    Add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="app">
+    <h1>Hello Vue!</h1>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
-  }
 });
 </script>
 
